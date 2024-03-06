91f434304e29842d8de8ad63a4174a67524eefee
ch-2020 <cheehung.koo@gmail.com>
2024-03-06 10:10:35 +0100

    Updated cargo.toml and added script to find doc

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sandbox : Interacting with mongodb in Rust
+
+This sandbox aims to test functionalities of mongodb using Rust. This is based on the official tutorial of MongoDB https://www.mongodb.com/docs/drivers/rust/current/usage-examples/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use mongodb::{
+    bson::doc,
+    Client,
+    Collection
+};
+use serde::{ Deserialize, Serialize };
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Restaurant {
+    name: String, 
+    cuisine: String,
+}
+
+// Finding a document from database
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
+    let uri = "<connection_string>";
+    let client = Client::with_uri_str(uri).await?;
+    
+    let my_coll: Collection<Restaurant> = client
+        .database("sample_restaurants")
+        .collection("restaurants");
+
+    let result = my_coll.find_one(
+        doc! { "name": "Tompkins Square Bagels" },
+        None
+    ).await?;
+
+    println!("{:#?}", result);
+    
+    Ok(())
 }
